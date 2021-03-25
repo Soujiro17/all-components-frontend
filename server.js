805218a629746cb6e473668869ae5bde3ff1101d
Soujiro17 <vicente_tomi@hotmail.com>
2021-03-25 12:57:19 -0300

    agregado todinho

---
+++ server.js
@@ -0,0 +1,105 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+const path = require("path");
+const port = process.env.PORT;
+const fs = require("fs");
+var cors = require('cors');
+
+const info = require('./routes/route');
+
+
+function jsonReader(filePath, cb) {
+    fs.readFile(filePath, (err, fileData) => {
+        if (err) {
+            return cb && cb(err)
+        }
+        try {
+            const object = JSON.parse(fileData)
+            return cb && cb(null, object)
+        } catch(err) {
+            return cb && cb(err)
+        }
+    })
+}
+
+app.use(morgan('dev'));
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+app.use(cors());
+app.use("/data/", info)
+app.set('json spaces', 2)
+
+app.get("/", (req, res) => {
+  const filePath = path.resolve(__dirname, `./build`, "index.html");
+  fs.readFile(filePath, "utf8", (err, data) => {
+    if (err) {
+      return console.log(err);
+    }
+    
+    res.send(data)
+  });
+});
+
+app.use(express.static(path.resolve(__dirname, `./build`)));
+
+app.post("/", (req, res) => {
+    const data = req.body
+
+    console.log(data)
+
+    jsonReader('./data.json', (err, customer) => {
+        if (err) {
+            console.log('Error reading file:',err)
+            return
+        }
+
+        for(let x = 0; x<data.products.length; x++){
+            if(customer.products.includes(data.products[x])){
+                const index = customer.products.indexOf(data.products[x])
+                customer.prices[index] = data.prices[x]
+                customer.stock[index] = data.stock[x]
+            }else{
+                customer.products.push(data.products[x]) 
+                customer.prices.push(data.prices[x]) 
+                customer.links.push(data.links[x]) 
+                customer.stock.push(data.stock[x]) 
+            }
+        }
+    fs.writeFile('./data.json', JSON.stringify(customer, null, 2), (err) => {
+            if (err) console.log('Error writing file:', err)
+        })
+    })
+
+    console.log("received")
+    res.send("received")
+    
+})
+
+app.post("/del", (req, res) => {
+    const data = req.body
+
+    const empty = { 
+        "products": [], 
+        "prices": [], 
+        "links": [], 
+        "stock": [] 
+    }
+
+    fs.writeFile('./data.json', JSON.stringify(empty, null, 2), (err) => {
+            if (err) console.log('Error writing file:', err)
+    })
+    
+    console.log("deleted")
+    return res.send("deleted")
+
+    
+})
+
+
+
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
---
+++ src/components/tabla.jsx
@@ -0,0 +1,55 @@
+import React, { useEffect, useState } from 'react';
+import { XGrid } from '@material-ui/x-grid';
+import axios from 'axios'
+
+const columns = [
+    { field: 'id', headerName: 'id', width: 50 },
+    { field: 'productName', headerName: 'Producto', width: 600 },
+    { field: 'priceProduct', headerName: 'Precio', width: 130 },
+    { field: 'linkProduct', headerName: 'Link', width: 900 },
+    { field: 'stockProduct', headerName: 'Stock', width: 150 },
+];
+
+const toRow = (data) => {
+    let row = []
+
+    if(data === undefined){
+      return row
+    }
+
+    for(let x = 0; x<data.products.length; x++){
+        row.push({ id: x, productName: data.products[x], priceProduct: parseInt(data.prices[x]), linkProduct: data.links[x], stockProduct: data.stock[x]})
+    }
+    return row
+}
+
+export default function Tabla() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetchingData()
+  }, [])
+
+  const fetchingData = async () => {
+    const headers = {
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin' : '*',
+      'Access-Control-Allow-Methods' : 'GET,PUT,POST,DELETE,PATCH,OPTIONS'
+    }
+    await axios.get('http://localhost:3000/data', {headers: headers}).then(response => {
+        setData(toRow(response.data))
+    });
+  }
+
+
+
+  return (
+    <div style={{ height: '100vh', width: '100%' }}>
+      <XGrid
+        rows={data} 
+        columns={columns} 
+        pageSize={15} 
+        />
+    </div>
+  );
+}
