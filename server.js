1b6c49e542806b9d89a819a11508b1195f343bea
Soujiro17 <vicente_tomi@hotmail.com>
2021-03-25 21:42:19 -0300

    updated max file size

--- server.js
+++ server.js
@@ -28,6 +28,7 @@ app.use(morgan('dev'));
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 app.use(cors());
+app.use(express.bodyParser({limit: '50mb'}));
 app.use("/data/", info)
 app.set('json spaces', 2)
 
