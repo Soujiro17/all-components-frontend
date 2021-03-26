b4cfcea0e12bd7753a31870b841f4244c6480cda
Soujiro17 <vicente_tomi@hotmail.com>
2021-03-26 15:02:20 -0300

    fixed /data

--- server.js
+++ server.js
@@ -7,10 +7,7 @@ const port = process.env.PORT;
 const fs = require("fs");
 const cors = require('cors');
 const bodyParser = require('body-parser');
-
-const info = require('./routes/route');
-
-
+const data = require('./data.json');
 
 // ----------------------- FUNCTIONS ----------------------//
 
@@ -54,7 +51,6 @@ app.use(bodyParser.json({limit: '50mb'}));
 app.use(bodyParser.urlencoded({limit: '50mb', extended: true, parameterLimit: 1000000}));
 app.use(express.json());
 app.use(cors());
-app.use("/data/", info)
 app.set('json spaces', 2)
 
 
@@ -69,6 +65,10 @@ app.get("/", (req, res) => {
     
     res.send(data)
   });
+});
+
+app.get("/data", verifyToken, (req, res) => {
+    res.json(data)
 });
 
 app.use(express.static(path.resolve(__dirname, `./build`)));
