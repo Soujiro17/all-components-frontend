7b1b924c25ce2df963aa7e2dbdf77b4476abd4ce
Soujiro17 <vicente_tomi@hotmail.com>
2021-04-12 12:22:22 -0400

    header fixed

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -14,9 +14,8 @@ export default function Routes() {
                 <Route exact path = "/" component = {Home} />
                 <Route exact path = "/contact" component = {Contact} />
                 <Route exact path = "/list-pages" component = {ListPages} />
-                <Route exact path = "/admin" component = {Admin}/>
                 {
-                    loggedIn? <Route exact path = "/dashboard" component = {Dashboard}/> : ""
+                    loggedIn? <Route exact path = "/dashboard" component = {Dashboard}/> : <Route exact path = "/admin" component = {Admin}/>
                 }
                 <Redirect to = "/"/>
             </Switch>
